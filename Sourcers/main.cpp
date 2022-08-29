322b349359fb91675f748f3ead0e11edf43fe18d
arthurpcardoso <tuco1301@gmail.com>
2022-08-29 16:47:11 -0300

    criacao de arquivos

---
+++ Sourcers/main.cpp
@@ -0,0 +1,14 @@
+// Exemplo de uso de template list e de template vector.
+
+#include <stdexcept>
+#include <iostream>
+#include <string>
+#include <list>
+#include <vector>
+
+#include "../Headers/dominios.h"
+#include "../Headers/entidades.h"
+
+using namespace std;
+
+int main() {}
